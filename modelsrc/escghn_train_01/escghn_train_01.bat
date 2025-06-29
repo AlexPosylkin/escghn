874eb8558330a3acaf7266987d5c30ce8c57eeb3
Alexander <alex@posylkin.com>
2025-06-29 21:46:07 +0300

    Delete modelsrc directory

--- modelsrc/escghn_train_01/escghn_train_01.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-REM Compiling using the Wall Worm Batch File...
-"G:/Steam/steamapps/common/Half-Life 2/bin/studiomdl.exe" -game "G:/Steam/steamapps/sourcemods/escghn" -nop4 "G:/Steam/steamapps/common/Half-Life 2/sourcesdk_content/hl2/modelsrc/escghn/escghn_train_01.qc"
-@pause
--- modelsrc/escghn_turretbase/escghn_turretbase.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-REM Compiling using the Wall Worm Batch File...
-"G:/Steam/steamapps/common/Half-Life 2/bin/studiomdl.exe" -game "G:/Steam/steamapps/common/Half-Life 2/hl2" -nop4 "G:/Steam/steamapps/common/Half-Life 2/sourcesdk_content/hl2/modelsrc/escghn/escghn_turretbase.qc"
-@pause
